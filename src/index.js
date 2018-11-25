b1adb9ab258fb93370ff9239f23d78d61765960d
Ramon Cardena <info@ramoncardena.com>
2018-11-25 19:36:50 +0100

    Added webfontloader to get Raleway font.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import * as serviceWorker from './serviceWorker';
 
 import './utils/i18n';
 
+// This will make sure WebFont.load is only used in the browser.
+if (typeof window !== 'undefined') {
+    var WebFont = require('webfontloader');
+    
+    WebFont.load({
+    google: {
+        families: ['Raleway:200,300,400,500,600', 'sans-serif']
+    }
+    });
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
