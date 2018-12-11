d4cac204142a720aca42611858c1baa511a8203b
Ramon Cardena <info@ramoncardena.com>
2018-12-11 17:20:14 +0100

    I18nextProvider added.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import App from './App';
 import { loadState, saveState } from './utils/localStorage';
 import * as serviceWorker from './serviceWorker';
 
-import './utils/i18n';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './utils/i18n';
 
 // This will make sure WebFont.load is only used in the browser.
 if (typeof window !== 'undefined') {
@@ -30,11 +31,19 @@ store.subscribe(() => {
 });
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <I18nextProvider
+        i18n={i18n}
+        initialI18Store={window.initialI18Store}
+        initialLanguage={window.initialLanguage}
+    >
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </I18nextProvider>,
     document.getElementById('root')
 );
+
+if (module.hot) module.hot.accept();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
